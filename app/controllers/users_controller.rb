bb6b3470cad18e4c550bfaff2da5087fbabb7495
OanaMariaB <oana.berentan@yahoo.com>
2020-08-13 21:37:50 +0100

    show route and users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,13 +24,35 @@ class UsersController < ApplicationController
 
     end
 
+    #this route job is to render the sign up form
     get '/signup' do
-
+        #erb should render a view
+        erb :signup
     end
 
-    #user show page
+    post '/users' do
+        #here is where we will create a new user and persist the new user to the database
+        #I only want to persisit an user that has a name, email, password
+        if params[:name] !="" && params[:email] !="" && params[:password] !=""
+            @user = User.create(params)
+            #I want to go to the user show page now
+            redirect "/users/#{@user.id}"
+        else   
+            #maybe include a message as a strech goal telling the user there's an error
+            redirect "/signup"
+        end
+    end
+
+    #user show route
     get '/users/:id' do
-        "this will be the users show route"
+        @user = User.find_by(id: params[:id])
+        session[:user_id] = @user.id
+       erb :'/users/show'
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 
 end
