4f538a97e5c3b336494be9328496d2f461b75f02
OanaMariaB <oana.berentan@yahoo.com>
2020-08-21 21:40:52 +0100

    mount the recipe entries controller

---
+++ app/controllers/recipe_entries_ controller.rb
@@ -0,0 +1,11 @@
+class RecipeEntriesController < ApplicationController
+
+    #get recipe_entries/new to render a form to create new entry
+
+    #post recipe_entries to create a new recipe entry
+
+    #show page for a recipe entry
+
+    #index route for all recipe entries
+    
+end
