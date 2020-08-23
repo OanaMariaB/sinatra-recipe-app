669004a5ec8ef2955edc37c5c18221e014464bf6
OanaMariaB <oana.berentan@yahoo.com>
2020-08-23 16:11:22 +0100

    add recipe controller

--- app/controllers/recipe_entries_ controller.rb
+++ app/controllers/recipe_entries_ controller.rb
@@ -2,10 +2,18 @@ class RecipeEntriesController < ApplicationController
 
     #get recipe_entries/new to render a form to create new entry
 
+    get '/recipe_entries/new' do
+        erb :'recipe_entries/new'
+
+    end
+
     #post recipe_entries to create a new recipe entry
+    # post route should not render anything, but to create a new entry and redirect
+
+
 
     #show page for a recipe entry
 
     #index route for all recipe entries
-    
+
 end
